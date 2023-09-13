1d624a585a9388fe3d930d54411b18b2a01f44a8
alejandrocora <alzapicolara@gmail.com>
2023-09-13 20:04:46 +0200

    fixed firefox

--- bizleaker/utils/selaux.py
+++ bizleaker/utils/selaux.py
@@ -4,6 +4,7 @@ from selenium.webdriver.chrome.options import Options as chrome_Options
 from selenium.webdriver.firefox.options import Options as firefox_Options
 
 def headless_chrome():
+    print('Chrom')
     options = chrome_Options()
     options.add_argument('--headless')
     options.add_argument("user-agent=Mozilla/5.0 (X11; Linux x86_64; rv:105.0) Gecko/20100101 Firefox/105.0")
@@ -16,5 +17,5 @@ def headless_firefox():
     options.add_argument('--headless')
     options.add_argument("user-agent=Mozilla/5.0 (X11; Linux x86_64; rv:105.0) Gecko/20100101 Firefox/105.0")
     options.add_argument("--window-size=1280,800")
-    driver = webdriver.Firefox(options=options, service_log_path=os.devnull)
+    driver = webdriver.Firefox(options=options)
     return driver
